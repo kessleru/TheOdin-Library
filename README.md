5f777fe0b907716da54a83804e761b83c4064e11
kessleru <otaviokesslerustra@hotmail.com>
2025-12-08 22:15:23 -0300

    Add book management functionality with modal and display updates

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-under construction ...
+todo:
+-img com uma cor para cada livro
+-modal info
+-read e unread
+-separar em modulos
+-Responsivo, principalmente celular
+-REFATORAR TUDO
+
+A ideia nao é ser um projeto bem organizado e estruturado.
--- script.js
+++ script.js
@@ -15,3 +15,63 @@ function foraModal(event) {
     toggleModal();
   }
 }
+
+const titleInput = document.getElementById('title');
+const authorInput = document.getElementById('author');
+const form = document.getElementById('add');
+const books = document.getElementById('books');
+
+let myLibrary = [];
+
+class Book {
+  constructor(title, author) {
+    this.title = title;
+    this.author = author;
+  }
+}
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  addBookToLibrary(titleInput.value, authorInput.value);
+
+  form.reset();
+  toggleModal();
+});
+
+function addBookToLibrary(title, author) {
+  const newBook = new Book(title, author);
+
+  myLibrary.push(newBook);
+
+  displayBook(newBook);
+}
+
+function displayBook(book) {
+  const bookItem = document.createElement('li');
+  bookItem.classList.add('book-item');
+
+  const cover = document.createElement('div');
+  cover.classList.add('book-cover');
+  cover.innerHTML = '<img src="img/livro2.png" alt="capa de livro" />';
+
+  const title = document.createElement('div');
+  title.classList.add('book-title');
+  title.innerHTML = `<h2>${book.title}</h2>`;
+
+  const author = document.createElement('div');
+  author.classList.add('book-author');
+  author.innerHTML = `<h2>${book.author}</h2>`;
+
+  const info = document.createElement('div');
+  info.classList.add('book-info');
+  info.innerHTML =
+    '<a href=""><img src="img/info.svg" alt="info de um livro"/></a>';
+
+  bookItem.appendChild(cover);
+  bookItem.appendChild(title);
+  bookItem.appendChild(author);
+  bookItem.appendChild(info);
+
+  books.appendChild(bookItem);
+}
